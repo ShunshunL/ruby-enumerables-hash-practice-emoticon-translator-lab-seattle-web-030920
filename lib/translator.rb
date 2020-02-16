714c4f3dd5143a40d08ce9d63a441e30517892e9
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-16 19:23:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 def load_library(intake)
   load_file = YAML.load_file(intake)
   result = {get_meaning: {}, get_emoticon: {}}
-  
+  load_file.do each 
 end
 
 def get_japanese_emoticon
