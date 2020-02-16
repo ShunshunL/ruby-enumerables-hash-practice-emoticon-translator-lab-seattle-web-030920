9f1637b8e9eb0f0343038366e3734e8548863986
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-16 19:35:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(intake)
   result = {get_meaning: {}, get_emoticon: {}}
   load_file.each do |meaning, emoticons| 
     result[:get_meaning][emoticons[1]] = meaning
-    result[:get_emoticon][emoticon[0]] = emoticon[1] 
+    result[:get_emoticon][emoticons[0]] = emoticons[1] 
   end
   result
 end
