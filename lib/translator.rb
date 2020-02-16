d4791049b73902857f93df83d77352326106e59a
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-16 19:39:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library(intake)
 end
 
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
-  
+  library = load_library(file_path);
 end
 
 def get_english_meaning
